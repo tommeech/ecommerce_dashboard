8ccf11d53e0a298351df48439c467d8e0b5cdf92
tommeech <tom.meech@sap.com>
2024-01-16 11:03:04 +0000

    added database

--- app.py
+++ app.py
@@ -1,3 +1,12 @@
 from flask import Flask, jsonify, render_template, Response
 import sqlite3
 import pathlib
+
+working_directory = pathlib.Path(_file_).parent.absolute()
+DATABASE = working_directory / 'CCL_ecommerce.db'
+
+def query_db(query: str, args=()) -> list:
+    with sqlite3.connect(DATABASE) as conn:
+        cursor = conn.cursor ()
+        result = cursor.execute (query, args).fetchall()
+    return result
