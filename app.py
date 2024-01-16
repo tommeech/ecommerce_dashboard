31dca52a4ed8c00e0fa95d6026a048404b8f2eb8
tommeech <tom.meech@sap.com>
2024-01-16 11:06:48 +0000

    adjusted _file_ to be __file__

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from flask import Flask, jsonify, render_template, Response
 import sqlite3
 import pathlib
 
-working_directory = pathlib.Path(_file_).parent.absolute()
+working_directory = pathlib.Path(__file__).parent.absolute()
 DATABASE = working_directory / 'CCL_ecommerce.db'
 
 def query_db(query: str, args=()) -> list:
