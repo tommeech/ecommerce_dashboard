9ba21d0bcf336177538723c8c4710696d04a2c00
tommeech <tom.meech@sap.com>
2024-01-16 11:19:47 +0000

    set up flask servers created app routes created api endpoints

--- app.py
+++ app.py
@@ -10,3 +10,25 @@ def query_db(query: str, args=()) -> list:
         cursor = conn.cursor ()
         result = cursor.execute (query, args).fetchall()
     return result
+
+app = Flask(__name__)
+
+@app.route('/')
+def index() -> str:
+    return render_template('dashboard.html')
+
+@app.route("/api/orders_over_time")
+def orders_over_time() -> Response:
+
+    query = """
+    SELECT order_date, COUNT(order_id) AS num_orders
+    FROM orders
+    GROUP BY order_date
+    ORDER BY order_date;
+    """
+
+    result = query_db(query)
+    
+    dates = [row[0] for row in result]
+    counts = [row[1] for row in result]
+    return jsonify({"dates": dates, "counts": counts})
